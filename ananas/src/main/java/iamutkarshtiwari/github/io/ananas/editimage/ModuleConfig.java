f1b2b1c6a1ddb60dd49857734e0664fb3e3db204
Pavel Atanassov <pavel.atanassov@outlook.com>
2026-05-05 16:25:36 +0200

    [SNAP-3959]

--- ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/ModuleConfig.java
+++ ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/ModuleConfig.java
@@ -2,14 +2,14 @@ package iamutkarshtiwari.github.io.ananas.editimage;
 
 public final class ModuleConfig {
     public static final int INDEX_MAIN = 0;
-    public static final int INDEX_STICKER = 1;
-    public static final int INDEX_SHAPE = 2;
-    public static final int INDEX_FILTER = 3;
-    public static final int INDEX_CROP = 4;
-    public static final int INDEX_ROTATE = 5;
-    public static final int INDEX_ADDTEXT = 6;
-    public static final int INDEX_PAINT = 7;
-    public static final int INDEX_BEAUTY = 8;
-    public static final int INDEX_BRIGHTNESS = 9;
-    public static final int INDEX_CONTRAST = 10;
+    public static final int INDEX_PAINT = 1;
+    public static final int INDEX_CROP = 2;
+    public static final int INDEX_ROTATE = 3;
+    public static final int INDEX_SHAPE = 4;
+    public static final int INDEX_ADDTEXT = 5;
+    public static final int INDEX_STICKER = 6;
+    public static final int INDEX_FILTER = 7;
+    public static final int INDEX_BRIGHTNESS = 8;
+    public static final int INDEX_CONTRAST = 9;
+    public static final int INDEX_BEAUTY = 10;
 }
