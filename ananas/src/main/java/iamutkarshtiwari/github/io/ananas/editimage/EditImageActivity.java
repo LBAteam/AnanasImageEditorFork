9fad44d860b2f840c41e0d553c585bb47082d85b
Pavel Atanassov <pavel.atanassov@outlook.com>
2026-05-09 21:38:22 +0200

    [SNAP-3958]

--- ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/EditImageActivity.java
+++ ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/EditImageActivity.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -422,10 +423,15 @@ public class EditImageActivity extends BaseActivity implements OnLoadingDialogLi
 
             if (shouldShowKeyboard) {
                 captionEditText.requestFocus();
+                if (dialog.getWindow() != null) {
+                    dialog.getWindow().setSoftInputMode(
+                            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                }
                 InputMethodManager inputMethodManager =
                         (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 if (inputMethodManager != null) {
-                    inputMethodManager.showSoftInput(captionEditText, InputMethodManager.SHOW_IMPLICIT);
+                    captionEditText.post(() ->
+                            inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0));
                 }
             }
         });
--- ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/model/ShapeCatalog.java
+++ ananas/src/main/java/iamutkarshtiwari/github/io/ananas/editimage/model/ShapeCatalog.java
@@ -6,64 +6,64 @@ public final class ShapeCatalog {
     private static final StickerPack[] PACKS = new StickerPack[]{
             new StickerPack(
                     R.string.iamutkarshtiwari_github_io_ananas_shape_pack_arrows,
-                    R.drawable.shape_arrow_01,
-                    R.drawable.shape_arrow_02,
-                    R.drawable.shape_arrow_03,
-                    R.drawable.shape_arrow_04,
-                    R.drawable.shape_arrow_05,
-                    R.drawable.shape_arrow_06,
-                    R.drawable.shape_arrow_07,
-                    R.drawable.shape_arrow_08,
-                    R.drawable.shape_arrow_09,
-                    R.drawable.shape_arrow_10
+                    R.drawable.shape_arrow_01, // red thick right
+                    R.drawable.shape_arrow_02, // red thick both directions
+                    R.drawable.shape_arrow_05, // red simple right
+                    R.drawable.shape_arrow_06, // red simple thick both directions
+                    R.drawable.shape_arrow_09, // red very thick right
+                    R.drawable.shape_arrow_03, // blue thick right
+                    R.drawable.shape_arrow_04, // blue thick both directions
+                    R.drawable.shape_arrow_07, // blue simple right
+                    R.drawable.shape_arrow_08, // blue simple thick both directions
+                    R.drawable.shape_arrow_10  // blue very thick right
             ),
             new StickerPack(
                     R.string.iamutkarshtiwari_github_io_ananas_shape_pack_numbers,
-                    R.drawable.shape_number_01,
-                    R.drawable.shape_number_02,
-                    R.drawable.shape_number_03,
-                    R.drawable.shape_number_04,
-                    R.drawable.shape_number_05,
-                    R.drawable.shape_number_06,
-                    R.drawable.shape_number_07,
-                    R.drawable.shape_number_08,
-                    R.drawable.shape_number_09,
-                    R.drawable.shape_number_10,
-                    R.drawable.shape_number_11,
-                    R.drawable.shape_number_12,
-                    R.drawable.shape_number_13,
-                    R.drawable.shape_number_14,
-                    R.drawable.shape_number_15,
-                    R.drawable.shape_number_16,
-                    R.drawable.shape_number_17,
-                    R.drawable.shape_number_18,
-                    R.drawable.shape_number_19,
-                    R.drawable.shape_number_20,
-                    R.drawable.shape_number_21,
-                    R.drawable.shape_number_22,
-                    R.drawable.shape_number_23,
-                    R.drawable.shape_number_24
+                    R.drawable.shape_number_01, // red 1
+                    R.drawable.shape_number_02, // red 2
+                    R.drawable.shape_number_03, // red 3
+                    R.drawable.shape_number_04, // red 4
+                    R.drawable.shape_number_05, // red 5
+                    R.drawable.shape_number_06, // red 6
+                    R.drawable.shape_number_07, // red 7
+                    R.drawable.shape_number_08, // red 8
+                    R.drawable.shape_number_09, // red 9
+                    R.drawable.shape_number_10, // red 0
+                    R.drawable.shape_number_21, // red +
+                    R.drawable.shape_number_22, // red -
+                    R.drawable.shape_number_11, // blue 1
+                    R.drawable.shape_number_12, // blue 2
+                    R.drawable.shape_number_13, // blue 3
+                    R.drawable.shape_number_14, // blue 4
+                    R.drawable.shape_number_15, // blue 5
+                    R.drawable.shape_number_16, // blue 6
+                    R.drawable.shape_number_17, // blue 7
+                    R.drawable.shape_number_18, // blue 8
+                    R.drawable.shape_number_19, // blue 9
+                    R.drawable.shape_number_20, // blue 0
+                    R.drawable.shape_number_23, // blue +
+                    R.drawable.shape_number_24  // blue -
             ),
             new StickerPack(
                     R.string.iamutkarshtiwari_github_io_ananas_shape_pack_other,
-                    R.drawable.shape_other_01,
-                    R.drawable.shape_other_02,
-                    R.drawable.shape_other_03,
-                    R.drawable.shape_other_04,
-                    R.drawable.shape_other_05,
-                    R.drawable.shape_other_06,
-                    R.drawable.shape_other_08,
-                    R.drawable.shape_other_09,
-                    R.drawable.shape_other_10,
-                    R.drawable.shape_other_11,
-                    R.drawable.shape_other_12,
-                    R.drawable.shape_other_13,
-                    R.drawable.shape_other_14,
-                    R.drawable.shape_other_15,
-                    R.drawable.shape_other_16,
-                    R.drawable.shape_other_18,
-                    R.drawable.shape_other_19,
-                    R.drawable.shape_other_20
+                    R.drawable.shape_other_01, // red circle
+                    R.drawable.shape_other_02, // red ellipse
+                    R.drawable.shape_other_03, // red square
+                    R.drawable.shape_other_04, // red rectangle
+                    R.drawable.shape_other_05, // red rounded square
+                    R.drawable.shape_other_06, // red rounded rectangle
+                    R.drawable.shape_other_08, // red triangle
+                    R.drawable.shape_other_09, // red star
+                    R.drawable.shape_other_10, // red pentagon
+                    R.drawable.shape_other_11, // blue circle
+                    R.drawable.shape_other_12, // blue ellipse
+                    R.drawable.shape_other_13, // blue square
+                    R.drawable.shape_other_14, // blue rectangle
+                    R.drawable.shape_other_15, // blue rounded square
+                    R.drawable.shape_other_16, // blue rounded rectangle
+                    R.drawable.shape_other_18, // blue triangle
+                    R.drawable.shape_other_19, // blue star
+                    R.drawable.shape_other_20  // blue pentagon
             )
     };
 
